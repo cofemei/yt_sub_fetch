9bba4aaf55a7b4d0b12b7149333e1e23d044959b
cofemei <cofemei@gmail.com>
2024-12-12 13:01:21 +0800

    fix lint

--- test/test_yt_sub_fetch.rb
+++ test/test_yt_sub_fetch.rb
@@ -1,12 +1,12 @@
-require 'minitest/autorun'
-require_relative 'test_helper'
+require "minitest/autorun"
+require_relative "test_helper"
 
 class YouTubeSubtitleFetchTest < Minitest::Test
   def test_parse_options_from_cli
-    args = ['-u', 'https://www.youtube.com/watch?v=testvideo', '-L']
+    args = ["-u", "https://www.youtube.com/watch?v=testvideo", "-L"]
     options = YtSubFetch::YouTubeSubtitleFetch.parse_options_from_cli(args)
 
-    assert_equal 'https://www.youtube.com/watch?v=testvideo', options[:url]
+    assert_equal "https://www.youtube.com/watch?v=testvideo", options[:url]
     assert options[:list]
     refute options[:all_languages]
     assert_nil options[:language]
@@ -14,9 +14,9 @@ class YouTubeSubtitleFetchTest < Minitest::Test
   end
 
   def test_extract_video_id
-    url = 'https://www.youtube.com/watch?v=0oNX_BHgi32'
+    url = "https://www.youtube.com/watch?v=0oNX_BHgi32"
     video_id = YtSubFetch::YouTubeSubtitleFetch.new({url: url}).extract_video_id(url)
 
-    assert_equal '0oNX_BHgi32', video_id
+    assert_equal "0oNX_BHgi32", video_id
   end
 end
